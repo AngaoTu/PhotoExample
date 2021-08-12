9175ae67cff8276a0069d8ffa65add58d40ffac9
AngaoTu <fetedream@foxmail.com>
2021-08-12 19:48:48 +0800

    PHCollectionListViewController

---
+++ PhotoExample/ViewController/AssetRetrieval/PHCollection/PHCollectionListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PHCollectionListViewController.swift
+//  PhotoExample
+//
+//  Created by AngaoTu on 2021/8/12.
+//
+
+import UIKit
+
+class PHCollectionListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
